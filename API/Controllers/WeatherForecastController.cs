dffc4a71165d251900ce644ba831dd620a25a6d1
Ceprnic <ceprnic.josip@gmail.com>
2024-01-10 19:21:23 +0100

    dotnet

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("[controller]")] // https://localhost:5001/WeatherForecast, controller je ignoriran
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
