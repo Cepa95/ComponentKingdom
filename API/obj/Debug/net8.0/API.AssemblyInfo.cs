5a2f5ebccf3d34fc0fc6e190f43095761e94884a
Ceprnic <ceprnic.josip@gmail.com>
2024-01-31 11:36:04 +0100

    auto mapper

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20ede228cf4f5944d35943eb42db491528a5c69a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b81366cabe88514e70377a7e6ed71e0fd8902b41")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
