1a92492342bda33a5d4febf2f3adbffe0cf199cc
Ceprnic <ceprnic.josip@gmail.com>
2024-02-04 00:56:27 +0100

    cleaning up the Startup class

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4238d527c3f4191e9d390eca04738cd97d470e04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2e7049327a0e685a1fff5dbf3951e87ad7b7384")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
