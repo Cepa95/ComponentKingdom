2357e101306035973fef643d8acdb639fbea2a7d
Ceprnic <ceprnic.josip@gmail.com>
2024-02-14 18:48:37 +0100

    organising files and folders

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,21 +5,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
-
+import { CoreModule} from './core/core.module'
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     NgbModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
