63a395431dc00035a9950f1b80d602961a3613df
Ceprnic <ceprnic.josip@gmail.com>
2024-02-21 21:24:32 +0100

    routing

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
   navbarOpen = false;
+
+  constructor(private router: Router) { }
 
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
   }
 
+  closeNavbar() {
+    if (this.navbarOpen) {
+      this.navbarOpen = false;
+    }
+  }
 }
