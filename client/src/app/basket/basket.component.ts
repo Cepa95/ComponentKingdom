89b75e5037d0f92f7b6695205ea971e7b0f6e60f
Ceprnic <ceprnic.josip@gmail.com>
2024-03-03 12:47:55 +0100

    basket functions

--- client/src/app/basket/basket.component.ts
+++ client/src/app/basket/basket.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { BasketService } from './basket.service';
+import { BasketItem } from '../shared/models/basket';
 
 @Component({
   selector: 'app-basket',
@@ -7,6 +8,13 @@ import { BasketService } from './basket.service';
   styleUrl: './basket.component.scss',
 })
 export class BasketComponent {
-basket: any;
   constructor(public basketService: BasketService) {}
+
+  incrementQuantity(item: BasketItem) {
+    this.basketService.addItemToBasket(item);
+  }
+
+  removeItem(id: number, quantity: number) {
+    this.basketService.removeItemFromBasket(id, quantity);
+  }
 }
