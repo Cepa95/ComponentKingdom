e8c6bc62cfb12a7f391e5d013e70a5c49c519042
Ceprnic <ceprnic.josip@gmail.com>
2024-03-21 10:27:52 +0100

    register user

--- Client/src/app/account/register/register.component.ts
+++ Client/src/app/account/register/register.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  errors: string[] | null = null;
 
+  constructor(
+    private fb: FormBuilder,
+    private accountService: AccountService,
+    private router: Router
+  ) {}
+
+  complexPassword =
+    "(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$";
+
+  registerForm = this.fb.group({
+    displayName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: [
+      '',
+      [Validators.required, Validators.pattern(this.complexPassword)],
+    ],
+    
+  });
+
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: () => this.router.navigateByUrl('/shop'),
+      error: (error) => {
+        if (error.status === 400 && error.error && error.error.errors) {
+          this.errors = error.error.errors;
+        } else {
+          this.errors = ['An error occurred. Please try again.'];
+        }
+      },
+    });
+  }
 }
