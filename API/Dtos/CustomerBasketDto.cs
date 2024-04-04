201a7f48318a34ab6cc8f18262219a9a9804e121
Ceprnic <ceprnic.josip@gmail.com>
2024-04-04 16:06:40 +0200

    stripe intent

--- API/Dtos/CustomerBasketDto.cs
+++ API/Dtos/CustomerBasketDto.cs
@@ -10,6 +10,10 @@ namespace API.Dtos
 
         [Required]
         public List<BasketItemDto> Items { get; set; }
+
+        public int? DeliveryMethodId { get; set; }
+        public string ClientSecret { get; set; }
+        public string PaymentIntentId { get; set; }
         
     }
 }
