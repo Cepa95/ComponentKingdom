600bce1fd95e3127078ee30f313973da32fe6e0e
Ceprnic <ceprnic.josip@gmail.com>
2024-04-06 13:47:48 +0200

    Persisting the shipping price

--- Client/src/app/checkout/checkout-review/checkout-review.component.ts
+++ Client/src/app/checkout/checkout-review/checkout-review.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { BasketService } from '../../basket/basket.service';
 
 @Component({
   selector: 'app-checkout-review',
   templateUrl: './checkout-review.component.html',
-  styleUrl: './checkout-review.component.scss'
+  styleUrl: './checkout-review.component.scss',
 })
 export class CheckoutReviewComponent {
+  constructor(private basketService: BasketService) {}
 
+  createPaymentIntent() {
+    this.basketService.createPaymentIntent().subscribe({
+      next: () => console.log('Payment Intent Created'),
+      error: (error) => console.log(error),
+    });
+  }
 }
--- client/src/app/checkout/checkout.component.ts
+++ client/src/app/checkout/checkout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { AccountService } from '../account/account.service';
+import { BasketService } from '../basket/basket.service';
 
 @Component({
   selector: 'app-checkout',
@@ -8,10 +9,15 @@ import { AccountService } from '../account/account.service';
   styleUrl: './checkout.component.scss',
 })
 export class CheckoutComponent implements OnInit {
-  constructor(private fb: FormBuilder, private accountService: AccountService) {}
+  constructor(
+    private fb: FormBuilder,
+    private accountService: AccountService,
+    private basketService: BasketService
+  ) {}
 
   ngOnInit(): void {
     this.getAddressFromValues();
+    this.getDeliveryMethodValue();
   }
   checkoutForm = this.fb.group({
     addressForm: this.fb.group({
@@ -27,14 +33,24 @@ export class CheckoutComponent implements OnInit {
     }),
     paymentForm: this.fb.group({
       nameOnCard: ['', Validators.required],
-    })
+    }),
   });
 
   getAddressFromValues() {
     this.accountService.getUserAddress().subscribe({
-      next: address => {
+      next: (address) => {
         address && this.checkoutForm.get('addressForm')?.patchValue(address);
-      }
-    })
+      },
+    });
+  }
+
+  getDeliveryMethodValue() {
+    const basket = this.basketService.getCurrentBasketValue();
+    if (basket && basket.deliveryMethodId) {
+      this.checkoutForm
+        .get('deliveryForm')
+        ?.get('deliveryMethod')
+        ?.patchValue(basket.deliveryMethodId.toString());
+    }
   }
 }
